e848501fec2d034acc7507b10055fd2f64678f17
PaulStreet <PaulStreet@users.noreply.github.com>
2015-06-10 14:08:01 -0400

    Update UpdatePredictiveLoad.vb

--- UpdatePredictiveLoad.vb
+++ UpdatePredictiveLoad.vb
@@ -1,4 +1,28 @@
 Sub UpdatePredictiveLoad()
+'This code enables the Predictive Load to be run with one click.
+'Coded by N99610 on 5/27/15
+
+    Dim importAP As Workbook
+    Dim importPress As Workbook
+    Dim predictiveLoad As Workbook
+    
+    Set predictiveLoad = ActiveWorkbook
+    
+    '## Open both workbooks first:
+    Set importAP = Workbooks.Open("PATH OMITTED")
+    Set importPress = Workbooks.Open("PATH OMITTED")
+    
+    importAP.Sheets("AllPlants").Range("A:CO").Copy
+    predictiveLoad.Sheets("AllPlants").Range("A:CO").PasteSpecial
+    Application.CutCopyMode = False
+    importAP.Close
+    
+    importPress.Sheets("Press").Range("A:AY").Copy
+    predictiveLoad.Sheets("Press").Range("A:AY").PasteSpecial
+    Application.CutCopyMode = False
+    importPress.Close
+    
+    predictiveLoad.Activate
 
     'This section adds an estimated weight and estimated feet column to the AllPlants tab.
     Sheets("AllPlants").Select
@@ -23,10 +47,9 @@ Sub UpdatePredictiveLoad()
     Dim PT As PivotTable
     Dim WS As Worksheet
     For Each WS In ThisWorkbook.Worksheets
-        For Each PT In WS.PivotTables
-          PT.RefreshTable
-          Range("A1").Select
-        Next PT
+            For Each PT In WS.PivotTables
+                PT.RefreshTable
+            Next PT
     Next WS
     
     'This section simply selects the CPM ES tab as the current worksheet at the end.
