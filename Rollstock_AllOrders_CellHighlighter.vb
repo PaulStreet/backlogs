bf3012d00563c9b39dfd625f9f725aeef4af6314
PaulStreet <PaulStreet@users.noreply.github.com>
2015-06-23 15:47:33 -0400

    Create Rollstock_AllOrders_CellHighlighter.vb

---
+++ Rollstock_AllOrders_CellHighlighter.vb
@@ -0,0 +1,9 @@
+Option Explicit
+ 
+Private Sub Worksheet_SelectionChange(ByVal Target As Range)
+'Code added to update the AllOrders to conditionally highlight rows and columns of active cell
+'Coded by N99610 on 6/23/15
+
+    Target.Calculate
+
+End Sub
